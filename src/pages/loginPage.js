9172194d40dffe086b0f1718b6606f48fef49d76
code-rks <rohitshaw7jan@gmail.com>
2024-06-27 20:44:42 +0530

    Integrated auth apis

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./loginPage.css";
+import { LoginUser } from "../api/user";
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
+  const navigate = useNavigate(); 
+  const [loginCred, setLoginCreds] = useState({
+    username: '',
+    password: ''
+  });
+  
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLoginCreds(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await LoginUser(loginCred.username, loginCred.password);
+    navigate('/checkin')
+  };
+
   return (
     <div className="login-page">
       <div className="login-container">
@@ -10,12 +32,15 @@ const LoginPage = () => {
           <h1 className="title">ConfConnect</h1>
         </div>
         <p className="description">Conference networking starts here!</p>
-        <form className="login-form">
-          <input type="text" placeholder="Username" className="input-field" />
+        <form className="login-form" onSubmit={handleSubmit}>
+          <input type="text" name="username" placeholder="Username" className="input-field" onChange={handleChange} required />
           <input
             type="password"
+            name="password"
             placeholder="Password"
             className="input-field"
+            onChange={handleChange}
+            required
           />
           <button type="submit" className="login-button">
             Log In
